a40338ff297da05d4adb55ba5dfd75562683685e
Bamlak <54115150+bamlak-kal@users.noreply.github.com>
2021-09-24 13:07:38 +0100

    Update AddNotice.js

--- src/components/tables/AddNotice.js
+++ src/components/tables/AddNotice.js
@@ -2,7 +2,6 @@ import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-// import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Button from '@material-ui/core/Button';
 
@@ -11,23 +10,11 @@ import './tables.css';
 
 const AddNotice = (props) => {
 
-    // const handleName = event => {
-    //     console.log(event.target.value)
-    //     props.setName(event.target.value);
-    // };
-
-    // const handleData = event => {
-    //     console.log(event.target.value)
-    //     // setData(event.target.value);
-    // };
 
     return (
         <Dialog fullWidth maxWidth="sm" open={props.open} aria-labelledby="form-dialog-title">
             <DialogTitle id="form-dialog-title">Add Notice</DialogTitle>
             <DialogContent>
-                {/* <DialogContentText>
-                    Enter notice details:
-                </DialogContentText> */}
                 <div className="form-container">
                     <TextField 
                         id="outlined-basic" 
